5bf304eda4477ce3b3002bfe8767656304620570
choijeongmi <checheda5@gmail.com>
2023-01-26 21:07:10 +0900

    아티클디테일 번호탐색

--- src/Y23M01D26.java
+++ src/Y23M01D26.java
@@ -50,17 +50,48 @@ public class Y23M01D26 {
 	        for(int i = articles.size() - 1; i >=0; i--) {
 	        	Article article = articles.get(i);
 	        	System.out.println("번호 | 제목");
-	        	System.out.printf("%d  |  %s\n", article.id, article.title);
+	        	System.out.printf("%d   |  %s\n", article.id, article.title);
 	        }
-	         
+	         	        
+	     }
 	        
-	        }
-	        
-	     
-	      else {
-	          System.out.println("존재하지 않는 명령어입니다.");
-	        
-	        }
+	      
+//	      startsWith > ~ 로 시작한다면.  / split (쪼갠다, 분할)
+	      else if(cmd.startsWith("article detail ")) {
+	    	  String[] cmdBits = cmd.split(" "); // 공백을 기준으로 분할할래 라는 뜻 "공백"
+//	    	  =>명령문이 3개의 분할이기 때문에 [] 배열로 묶어준다.
+	    	  int id = Integer.parseInt(cmdBits[2]);
+	    	  
+	    	  
+	    	  Article foundArticle = null;
+			for (int i = 0; i < articles.size(); i++) {
+	    		  Article article = articles.get(i);
+//	    		=> 0부터 size 전까지 탐색하겠다 라는 뜻. i = 0  
+	    		  if (article.id == id) {
+//	    			  found = true; > found 가 참일 경우 브레이크. 로 넣어줄수도 있음
+	    			  foundArticle = article;
+	    			  break; //  가장 가까운 반복문을 탈출하는 거. 탐색 중 정답을 찾았을때 탐색을 중지시키기위해
+	    		  }
+	    	  }
+//	    	위의  for문에서 탐색을 마친 후 정답을 찾으면 break를 통해 for문을 빠져나와 아래의  if문으로 이동.	
+//			  
+	    	  if (foundArticle == null) { // 찾은 게시물(foundArticle)이 없을때
+	    		  System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+	    		  continue;
+	    	  }    
+	    	    // 그 외의 값이면 아래가 출력 >>  외의값은 found  값이 트루일 경우 if가 아닌 else로 이동.
+	    		  System.out.printf("%d번 게시물은 존재합니다.\n", id);
+	    		  System.out.printf("번호 : %d\n", foundArticle.id);
+	    		  System.out.printf("날짜 :2022-01-26\n" );
+	    		  System.out.printf("제목 : %s\n", foundArticle.title);
+	    		  System.out.printf("내용 : %d\n", foundArticle.body);
+		        
+		        
+	   
+		        }
+	      
+//		     코드정리 -> 리팩토링이라고 한다.
+		  
 	      
 	      
 
