9128eba1bc5b24a7e91417dc5ca8e9f3edb0cb71
choijeongmi <checheda5@gmail.com>
2023-01-26 21:52:17 +0900

    자바15일차 게시판 만들기

--- src/Y23M01D26.java
+++ src/Y23M01D26.java
@@ -86,9 +86,34 @@ public class Y23M01D26 {
 	    		  System.out.printf("제목 : %s\n", foundArticle.title);
 	    		  System.out.printf("내용 : %d\n", foundArticle.body);
 		        
-		        
+//ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ		        
 	   
 		        }
+	      else if(cmd.startsWith("article delete ")) {
+	    	  String[] cmdBits = cmd.split(" "); 
+    	  
+	    	  int id = Integer.parseInt(cmdBits[2]);
+	    	  Article foundArticle = null;
+	    	  for (int i = 0; i < articles.size(); i++) {
+	    		  Article article = articles.get(i);
+    		 
+	    		  if (article.id == id) {
+	    			  foundArticle = article;
+	    			  break;
+	    		  }
+	    	  }
+	    	  if (foundArticle == null) {
+	    		  System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+	    		  continue;
+	    	  }
+	    	  articles.remove(id - 1);
+	    	  
+	    	  System.out.printf("%d번 게시물이 삭제 되었습니다.\n", id);
+
+	    	  
+	    	  
+	    	  
+	      }
 	      
 //		     코드정리 -> 리팩토링이라고 한다.
 		  
@@ -96,6 +121,7 @@ public class Y23M01D26 {
 	      
 
 	    }
+	    sc.close();
 	    System.out.println("==프로그램 종료==");
 	  }
 	}
